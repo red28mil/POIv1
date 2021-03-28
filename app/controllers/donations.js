c50c3c1481876de6b47b1faa3f2013d4f6883810
red28mil <maeliosa@live.com>
2021-03-28 11:44:48 +0100

    first

---
+++ app/controllers/donations.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Donations = {
+  home: {
+    handler: function (request, h) {
+      return h.view("home", { title: "Make a Donation" });
+    },
+  },
+  report: {
+    handler: function (request, h) {
+      return h.view("report", { title: "Donations so far" });
+    },
+  },
+};
+
+module.exports = Donations;
+
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const Handlebars = require('handlebars');
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+});
+
+
+async function init() {
+    await server.register(Inert);
+    await server.register(Vision);
+    server.views({
+        engines: {
+          hbs: require('handlebars'),
+        },
+        relativeTo: __dirname,
+        path: './app/views',
+        layoutPath: './app/views/layouts',
+        partialsPath: './app/views/partials',
+        layout: true,
+        isCached: false,
+      });
+  
+    server.route(require('./routes'));
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  }
+  
+  process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+  });
+  
+  init();
+
